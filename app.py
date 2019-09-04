b5fa89b409534f2bffc948e162ba4d0b277d9f12
zimkjh <zimkjh@naver.com>
2019-09-04 09:20:24 +0900

    changed config file. Can use matter (physics)

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, abort, request, render_template
 # app = Flask(__name__, static_folder="/home/ubuntu/wc/tutorial/static", static_url_path="/static", template_folder="/home/ubuntu/wc/tutorial/templates")
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,27 +20,10 @@ def part2():
 @app.route('/3')
 def part3():
 	return render_template('part3.html')
-@app.route('/4')
-def part4():
-	return render_template('part4.html')
-@app.route('/5')
-def part5():
-	return render_template('part5.html')
-@app.route('/6')
-def part6():
-	return render_template('part6.html')
-@app.route('/7')
-def part7():
-	return render_template('part7.html')
-@app.route('/8')
-def part8():
-	return render_template('part8.html')
-@app.route('/9')
-def part9():
-	return render_template('part9.html')
-@app.route('/10')
+
+@app.route('/tower_stacking')
 def part10():
-	return render_template('part10.html')
+	return render_template('tower_stacking.html')
 
 
 if __name__ == '__main__':
